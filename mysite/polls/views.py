3882a2a382556f254bccc158ecfcbf8d9781cf4f
Tomasz Kostur <tomasz.kostur@gmail.com>
2018-08-19 19:07:15 +0200

    Polls urls matching with views
    There are some new urlpatterns. The new pattherns passes arguments from
    '<>' to wiews.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world from Polls index")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question: {}".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at question: {}, results".format(question_id)
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse("Voting for question: {}".format(question_id))
