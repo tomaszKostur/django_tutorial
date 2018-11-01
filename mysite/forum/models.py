91498fa89d8dfbaa6e900c2c5502adbf90a2428b
Tomasz Kostur <tomasz.kostur@gmail.com>
2018-11-01 22:31:56 +0100

    Refresh training forum app.
    I had some issues after I manually deleted tables from database. It need
    to be investigated later.

--- mysite/forum/models.py
+++ mysite/forum/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-class User(models.Model):
+class Autor(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
 class Post(models.Model):
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     post_date = models.DateTimeField()
     post_text = models.TextField()
-#    autor = models.ForeignKey('User', on_delete=models.CASCADE)
-    invloved = models.ManyToManyField('User')
 
     def __str__(self):
         return self.post_text
--- mysite/forum/views.py
+++ mysite/forum/views.py
@@ -1,11 +1,11 @@
 import sys
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post, User
+from .models import Post, Autor
 
 def index_view(request):
     all_posts = Post.objects.all()
-    all_people = User.objects.all()
+    all_people = Autor.objects.all()
     try:
         new_post = Post(post_date=timezone.now(),
                         post_text=request.POST['name_of_text_input'])
